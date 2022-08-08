ce8fbe4d575536eb65009f7a9623e4a6e6fbde70
roargenie <105975078+roargenie@users.noreply.github.com>
2022-08-08 09:30:15 +0900

    Initial Commit

---
+++ SeSAC Week6 Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC Week6 Project
+//
+//  Created by 이명진 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
